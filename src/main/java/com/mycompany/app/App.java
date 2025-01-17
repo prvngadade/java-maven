009f1c3a84014cfa946e0cb11c8048a686b3ca38
prvngadade <64361721+prvngadade@users.noreply.github.com>
2025-01-17 12:36:06 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -14,10 +14,5 @@ public class App {
     public String getMessage() {
         return MESSAGE;
     }
-        public String getMessage() {
-        return MESSAGE;
-    }
-        public String getMessage() {
-        return MESSAGE;
-    }
 }  
+}
