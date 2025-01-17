223e40d8448caedc772092ec972d621264779b06
prvngadade <64361721+prvngadade@users.noreply.github.com>
2025-01-17 17:15:57 +0530

    Update test.java

--- src/main/java/com/mycompany/app/test.java
+++ src/main/java/com/mycompany/app/test.java
@@ -1,9 +1,13 @@
-//package org.example; // Compliant
+import java.util.logging.Logger;
+package org.example; // Compliant
 public class HelloWorld {
+    // Create a logger instance
+    private static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
+
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World");
+    public static void main(String[] args) {
+        // Use the logger to log the message
+        LOGGER.info("Hello, World");
     }
 }
