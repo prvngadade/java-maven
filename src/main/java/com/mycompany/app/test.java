3de4e331d9df73455bea63f35f88ebfffcde8574
prvngadade <64361721+prvngadade@users.noreply.github.com>
2025-01-17 16:31:45 +0530

    Update test.java

--- src/main/java/com/mycompany/app/test.java
+++ src/main/java/com/mycompany/app/test.java
@@ -1,4 +1,5 @@
-class HelloWorld {
+package org.example; // Compliant
+public class HelloWorld {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
